6be4e5eb601a115297b3a19a3f70cae4cc362d0d
ro-hits <rohitkp9645@gmail.com>
2024-01-16 10:20:35 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import ffmpeg
+import whisper
+
+def convert_time(time):
+    hours, remainder = divmod(time, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    milliseconds = int((time - int(time)) * 1000)
+    return f"{hours:02d}:{minutes:02d}:{seconds:02d},{milliseconds:03d}"
+
+
+def get_subtitle(video_path):
+
+    model = whisper.load_model("small")
+
+    # Extract audio from the video file using FFmpeg
+    audio_path = "audio.wav"
+    (
+        ffmpeg.input(video_path)
+        .output(audio_path, format="wav", acodec="pcm_s16le", ac=1, ar=16000)
+        .overwrite_output()
+        .run()
+    )
+
+    result = model.transcribe(audio_path)
+
+    video_filename = os.path.splitext(os.path.basename(video_path))[0]
+
+    subtitle_path = f"{video_filename}.srt"
+
+    with open(subtitle_path, "w") as f:
+        for i, caption in enumerate(result["segments"]):
+            start_time = convert_time(int(caption["start"]))
+            end_time = convert_time(int(caption["end"]))
+            text = caption["text"]
+            f.write(f"{i + 1}\n{start_time} --> {end_time}\n{text}\n\n")
+
+    # Remove the temporary audio file
+    os.remove(audio_path)
+    print("Subtitle saved as", subtitle_path)
+
+video_path = "test.mp4"  # Replace with the path to your video file
+get_subtitle(video_path)
